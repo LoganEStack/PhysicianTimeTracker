f4cff41490c0918301cefb756a79b0a44e3b482b
LiamGeoghan <ljg5222@psu.edu>
2018-03-13 12:40:47 -0400

    Add files via upload

---
+++ TimeTracker/PhysicianTimeTracker/PhysicianTimeTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhysicianTimeTracker
+//
+//  Created by Liam Geoghan on 3/13/18.
+//  Copyright © 2018 ShippertLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
